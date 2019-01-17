5676e161f91f32d997cb3113d94d76d86ed6709f
Fa.lourenco <fabiolourenco91@gmail.com>
2019-01-17 15:21:07 +0000

    PDF Downloadable

---
+++ Candidaturas/MigraDocument.cs
@@ -0,0 +1,215 @@
+﻿using MigraDoc.DocumentObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Candidaturas
+{
+    public class MigraDocument
+    {
+
+        /// <summary>
+        /// Defines the cover page.
+        /// </summary>
+        /// 
+        public static void DefineCover(Document document)
+        {
+            Section section = document.AddSection();
+            Paragraph paragraph = section.AddParagraph();
+            paragraph.Format.SpaceAfter = "3cm";
+
+            string ImgPath = ((new System.Uri(Assembly.GetExecutingAssembly().CodeBase)).AbsolutePath).Replace("bin/Candidaturas.DLL", "Content/img/marinha.png");
+            //section.AddImage("../../Content / img / marinha.png");
+            section.AddImage(ImgPath);
+
+            paragraph = section.AddParagraph("A sample document that demonstrates the\ncapabilities of MigraDoc");
+            paragraph.Format.Font.Size = 16;
+            paragraph.Format.Font.Color = Colors.DarkRed;
+            paragraph.Format.SpaceBefore = "8cm";
+            paragraph.Format.SpaceAfter = "3cm";
+            paragraph = section.AddParagraph(ImgPath);
+            paragraph = section.AddParagraph("Rendering date: ");
+            paragraph.AddDateField();
+        }
+
+        /// <summary>
+        /// Defines page setup, headers, and footers.
+        /// </summary>
+        static void DefineContentSection(Document document)
+        {
+            Section section = document.AddSection();
+            section.PageSetup.OddAndEvenPagesHeaderFooter = true;
+            section.PageSetup.StartingNumber = 1;
+
+            HeaderFooter header = section.Headers.Primary;
+            header.AddParagraph("\tOdd Page Header");
+
+            header = section.Headers.EvenPage;
+            header.AddParagraph("Even Page Header");
+
+            // Create a paragraph with centered page number. See definition of style "Footer".
+            Paragraph paragraph = new Paragraph();
+            paragraph.AddTab();
+            paragraph.AddPageField();
+
+            // Add paragraph to footer for odd pages.
+            section.Footers.Primary.Add(paragraph);
+            // Add clone of paragraph to footer for odd pages. Cloning is necessary because an object must
+            // not belong to more than one other object. If you forget cloning an exception is thrown.
+            section.Footers.EvenPage.Add(paragraph.Clone());
+        }
+
+        public static void DefineParagraphs(Document document)
+        {
+            Paragraph paragraph = document.LastSection.AddParagraph("Paragraph Layout Overview", "Heading1");
+            paragraph.AddBookmark("Paragraphs");
+
+
+
+            DemonstrateAlignment(document);
+            DemonstrateIndent(document);
+            DemonstrateFormattedText(document);
+            DemonstrateBordersAndShading(document);
+        }
+
+        static void DemonstrateAlignment(Document document)
+        {
+            string FillerText = "TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto";
+            document.LastSection.AddParagraph("Alignment", "Heading2");
+
+            document.LastSection.AddParagraph("Left Aligned", "Heading3");
+
+            Paragraph paragraph = document.LastSection.AddParagraph();
+            paragraph.Format.Alignment = ParagraphAlignment.Left;
+            paragraph.AddText(FillerText);
+
+            document.LastSection.AddParagraph("Right Aligned", "Heading3");
+
+            paragraph = document.LastSection.AddParagraph();
+            paragraph.Format.Alignment = ParagraphAlignment.Right;
+            paragraph.AddText(FillerText);
+
+            document.LastSection.AddParagraph("Centered", "Heading3");
+
+            paragraph = document.LastSection.AddParagraph();
+            paragraph.Format.Alignment = ParagraphAlignment.Center;
+            paragraph.AddText(FillerText);
+
+            document.LastSection.AddParagraph("Justified", "Heading3");
+
+            paragraph = document.LastSection.AddParagraph();
+            paragraph.Format.Alignment = ParagraphAlignment.Justify;
+            paragraph.AddText(FillerText);
+        }
+
+        static void DemonstrateIndent(Document document)
+        {
+            string FillerText = "TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto";
+            document.LastSection.AddParagraph("Indent", "Heading2");
+
+            document.LastSection.AddParagraph("Left Indent", "Heading3");
+
+            Paragraph paragraph = document.LastSection.AddParagraph();
+            paragraph.Format.LeftIndent = "2cm";
+            paragraph.AddText(FillerText);
+
+            document.LastSection.AddParagraph("Right Indent", "Heading3");
+
+            paragraph = document.LastSection.AddParagraph();
+            paragraph.Format.RightIndent = "1in";
+            paragraph.AddText(FillerText);
+
+            document.LastSection.AddParagraph("First Line Indent", "Heading3");
+
+            paragraph = document.LastSection.AddParagraph();
+            paragraph.Format.FirstLineIndent = "12mm";
+            paragraph.AddText(FillerText);
+
+            document.LastSection.AddParagraph("First Line Negative Indent", "Heading3");
+
+            paragraph = document.LastSection.AddParagraph();
+            paragraph.Format.LeftIndent = "1.5cm";
+            paragraph.Format.FirstLineIndent = "-1.5cm";
+            paragraph.AddText(FillerText);
+        }
+
+        static void DemonstrateFormattedText(Document document)
+        {
+            string FillerText = "TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto";
+            document.LastSection.AddParagraph("Formatted Text", "Heading2");
+
+            //document.LastSection.AddParagraph("Left Aligned", "Heading3");
+
+            Paragraph paragraph = document.LastSection.AddParagraph();
+            paragraph.AddText("Text can be formatted ");
+            paragraph.AddFormattedText("bold", TextFormat.Bold);
+            paragraph.AddText(", ");
+            paragraph.AddFormattedText("italic", TextFormat.Italic);
+            paragraph.AddText(", or ");
+            paragraph.AddFormattedText("bold & italic", TextFormat.Bold | TextFormat.Italic);
+            paragraph.AddText(".");
+            paragraph.AddLineBreak();
+            paragraph.AddText("You can set the ");
+            FormattedText formattedText = paragraph.AddFormattedText("size ");
+            formattedText.Size = 15;
+            paragraph.AddText("the ");
+            formattedText = paragraph.AddFormattedText("color ");
+            formattedText.Color = Colors.Firebrick;
+            paragraph.AddText("the ");
+            formattedText = paragraph.AddFormattedText("font", new MigraDoc.DocumentObjectModel.Font("Verdana"));
+            paragraph.AddText(".");
+            paragraph.AddLineBreak();
+            paragraph.AddText("You can set the ");
+            formattedText = paragraph.AddFormattedText("subscript");
+            formattedText.Subscript = true;
+            paragraph.AddText(" or ");
+            formattedText = paragraph.AddFormattedText("superscript");
+            formattedText.Superscript = true;
+            paragraph.AddText(".");
+        }
+
+        static void DemonstrateBordersAndShading(Document document)
+        {
+            string FillerText = "TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto";
+            document.LastSection.AddPageBreak();
+            document.LastSection.AddParagraph("Borders and Shading", "Heading2");
+
+            document.LastSection.AddParagraph("Border around Paragraph", "Heading3");
+
+            Paragraph paragraph = document.LastSection.AddParagraph();
+            paragraph.Format.Borders.Width = 2.5;
+            paragraph.Format.Borders.Color = Colors.Navy;
+            paragraph.Format.Borders.Distance = 3;
+            paragraph.AddText(FillerText);
+
+            document.LastSection.AddParagraph("Shading", "Heading3");
+
+            paragraph = document.LastSection.AddParagraph();
+            paragraph.Format.Shading.Color = Colors.LightCoral;
+            paragraph.AddText(FillerText);
+
+            document.LastSection.AddParagraph("Borders & Shading", "Heading3");
+
+            paragraph = document.LastSection.AddParagraph();
+            paragraph.Style = "TextBox";
+            paragraph.AddText(FillerText);
+        }
+
+        public static Document CreateDocument()
+        {
+            // Create a new MigraDoc document
+            Document PDFdocument = new Document();
+            PDFdocument.Info.Title = "Hello, MigraDoc";
+            PDFdocument.Info.Subject = "Demonstrates an excerpt of the capabilities of MigraDoc.";
+            PDFdocument.Info.Author = "Stefan Lange";
+            DefineCover(PDFdocument);
+            DefineContentSection(PDFdocument);
+            DefineParagraphs(PDFdocument);
+
+
+            return PDFdocument;
+        }
+    }
+}
