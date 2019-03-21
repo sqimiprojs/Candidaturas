188d91b82e26ea1ba0d77f61a8cf7986c4ef14d4
tiagocastanho6 <tiago.castanho@sqimi.com>
2019-03-21 12:51:09 +0000

    nif validar

--- Candidaturas/Scripts/DocumentValidation.js
+++ Candidaturas/Scripts/DocumentValidation.js
@@ -1,14 +1,7 @@
 ﻿$(document).ready(function () {
     $("#NIF").change(function () {
         var nif = $("#NIF").val();
-        if (validarNIF(nif)) {
-            $("#NIFWarning").text("");
-            $("#NIFWarning").hide();
-        }
-        else {
-            $("#NIFWarning").text("Numero de Identificação fiscal inválido");
-            $("#NIFWarning").show();
-        }
+        validarNIF(nif);
 
     });
 
@@ -315,14 +308,27 @@
     /* Formulario */
     function validarNIF(nif) {
 
-        if (!['1', '2', '3', '5', '6', '8'].includes(nif.substr(0, 1)) &&
-            !['45', '70', '71', '72', '77', '79', '90', '91', '98', '99'].includes(nif.substr(0, 2)))
+        if (!['1', '2', '3', '5', '6', '8'].includes(nif.substr(0, 1)) && !['45', '70', '71', '72', '77', '79', '90', '91', '98', '99'].includes(nif.substr(0, 2)))
+        {
+            $("#AvisoNIF").text("NIF INVALIDO");
+            $("#AvisoNIF").show();
             return false;
+        }
+   
         let total = nif[0] * 9 + nif[1] * 8 + nif[2] * 7 + nif[3] * 6 + nif[4] * 5 + nif[5] * 4 + nif[6] * 3 + nif[7] * 2;
 
         let modulo11 = total - parseInt(total / 11) * 11;
         let comparador = modulo11 == 1 || modulo11 == 0 ? 0 : 11 - modulo11;
-        return nif[8] == comparador;
+        if (nif[8] == comparador) {
+            $("#AvisoNIF").text("");
+            $("#AvisoNIF").hide();
+            return true;
+        }
+        else {
+            $("#AvisoNIF").text("NIF INVALIDO");
+            $("#AvisoNIF").show();
+            return false;
+        }
     }
 
     //limita 4 digitos
